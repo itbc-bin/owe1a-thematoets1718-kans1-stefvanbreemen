3bdc16851ccba4b992a7f12e5891051ee94223ac
stefvanbreemen <stefvanbreemen@hotmail.com>
2017-11-08 16:03:04 +0100

    verifier klaar

--- thematoets.py
+++ thematoets.py
@@ -1,27 +1,43 @@
 def main():
     list = []
     list = read(list)
-    ver(list)
-    print(list)
-
-
+    list = verifier(list)
+    very = []
+    vern = []
+    #ions(list)
 def read(list):
     bestand = open("yeast_genes.csv")
     list = []
     for line in bestand:
-        line = line.split()
+        line = line.split(",")
         list.append(line)
+    del(list[0])
     return list
 
-def ver(list):
+
+def verifier(list):
     very = []
     vern = []
+    y = 0
+    n = 0
+
     for i in list:
-        if i in list:
-            if i[1] == "verified":
-                very.append(i[0])
-            else:
-                vern.append(i[0])
-    print("verified:", very)
-    print("not Verified:", vern)
+        if i[1] == "Verified":
+            very.append(i[0])
+            y += 1
+        else:
+            vern.append(i[0])
+            n += 1
+    print("verified:    ",y, very)
+    print("not Verified:", n, vern)
+
+
+def ions(list):
+    ions = []
+
+    for i in list:
+        if i[2] == "ion":
+            ions.append(i[0])
+    return(ions)
+
 main()
